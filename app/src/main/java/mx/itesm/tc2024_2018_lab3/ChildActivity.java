68ae22a769c597160b1c7f3d229639b5c41892af
luisrcorral@gmail.com <luisrcorral@gmail.com>
2018-02-01 19:50:17 -0600

    Visibility example

--- app/src/main/java/mx/itesm/tc2024_2018_lab3/ChildActivity.java
+++ app/src/main/java/mx/itesm/tc2024_2018_lab3/ChildActivity.java
@@ -3,6 +3,7 @@ package mx.itesm.tc2024_2018_lab3;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 public class ChildActivity extends AppCompatActivity {
@@ -18,5 +19,6 @@ public class ChildActivity extends AppCompatActivity {
         TextView myTextView;
         myTextView = (TextView)findViewById(R.id.textView2);
         myTextView.setText(myText);
+        myTextView.setVisibility(View.INVISIBLE); // Demo of visibility
     }
 }
